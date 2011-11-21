f46fbfde932a0a84a8acad89ce80959547435523
David Medina <davidmedina9@gmail.com>
2011-11-21 14:32:08 +0100

    Initial commit

---
+++ compass_wrapper.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import subprocess
+import os
+
+class CompassWrapper(object):
+
+    def __init__(self, config=None):
+        pass
+
+    def _load_args(self):
+        pass
+
+    def __call__(self):
+        pass
+
+    @property
+    def compiler(self):
+        pass
+
+    def compile(self):
+        pass
+
+    def compile_file(self):
+        pass
