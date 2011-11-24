7ea868d973e5b1bb45287edda3ca7b943ca465e3
David Medina <davidmedina9@gmail.com>
2011-11-24 18:15:33 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='python-compass-wrapper',
+      version='0.1',
+      description='Wrapper around compass binary to execute with python',
+      author='David Medina',
+      author_email='davidmedina9@gmail.com',
+      url='https://github.com/wellnesstelecom/python-compass-wrapper',
+      packages=['compass_wrapper'],
+     )
